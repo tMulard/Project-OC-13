53aa9343aef03bdd74ec876f599d9eb88e3630f7
luc <luc@taketik.fr>
2025-09-30 15:00:20 +0200

    fix some bugs in update profile

--- front-end/store/slices/authSlice.js
+++ front-end/store/slices/authSlice.js
@@ -31,72 +31,76 @@ export const AuthSlice = createSlice({
     },
     logout: (state) => {
       state.isAuth = false,
-      state.token = null,
-      state.profile = null,
-      state.error = "",
-      state.successUpdate = false,
-      state.errorUpdate = null;
+        state.token = null,
+        state.profile = null,
+        state.error = "",
+        state.successUpdate = false,
+        state.errorUpdate = null;
     }
   },
   selectors: {
-    selectIsAuth : state => state.isAuth,
-    selectToken : state => state.token,
-    selectProfile : state => state.profile,
-    selectError : state => state.error,
-    selectSuccessUpdate : state => state.successUpdate,
-    selectErrorUpdate : state => state.errorUpdate
+    selectIsAuth: state => state.isAuth,
+    selectToken: state => state.token,
+    selectProfile: state => state.profile,
+    selectError: state => state.error,
+    selectSuccessUpdate: state => state.successUpdate,
+    selectErrorUpdate: state => state.errorUpdate
   }
 });
 
 export const logIn = (email, password) => async (dispatch) => {
-    
-    try {
-      const fetchData = await fetch("http://localhost:3001/api/v1/user/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
 
-      const response = await fetchData.json();
-      
-      if (response.status === 200 && response.body.token) {
-        dispatch(setIsAuth(true))
-        dispatch(setToken(response.body.token))
-      }
+  try {
+    const fetchData = await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
-      else if (response.status !== 200 && response.body.token) dispatch(setError(response.status))
-    
-    } catch (error) {
-      dispatch(setError(error.toString()));
+    const response = await fetchData.json();
+
+    if (response.status === 200 && response.body.token) {
+      dispatch(setIsAuth(true))
+      dispatch(setToken(response.body.token))
     }
+
+    else if (response.status !== 200 && response.body.token) dispatch(setError(response.status))
+
+  } catch (error) {
+    dispatch(setError(error.toString()));
+  }
 
 }
 
+export const { setIsAuth, setToken, setProfile, setError, logout, setSuccessUpdate, setErrorUpdate } = AuthSlice.actions;
+export const { selectIsAuth, selectToken, selectProfile, selectError, selectErrorUpdate, selectSuccessUpdate } = AuthSlice.selectors;
+
+
 export const getProfile = () => async (dispatch, getState) => {
-    try {
-      //console.log('state', getState())
-      const token = getState().auth.token;
-      //console.log('token', token)
-      const fetchData = await fetch(
-        "http://localhost:3001/api/v1/user/profile",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      const response = await fetchData.json();
-
-      if (response.status === 200) {
-        dispatch(setProfile(response.body));
+  try {
+    //console.log('state', getState())
+    const token = getState().auth.token;
+    //console.log('token', token)
+    const fetchData = await fetch(
+      "http://localhost:3001/api/v1/user/profile",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       }
-    } catch (error) {
-      dispatch(setError(error.toString()));
+    );
+
+    const response = await fetchData.json();
+
+    if (response.status === 200) {
+      dispatch(setProfile(response.body));
     }
-  };
+  } catch (error) {
+    dispatch(setError(error.toString()));
+  }
+};
 
 // formulaire d'update en TODO
 //si errorupdate -> erreur plus block
@@ -104,30 +108,31 @@ export const getProfile = () => async (dispatch, getState) => {
 
 export const upDate = (firstName, lastName) => async (dispatch, getState) => {
   try {
-      const token = getState().auth.token;
+    dispatch( setSuccessUpdate(false));
+    dispatch(setErrorUpdate(null));
+    const token = getState().auth.token;
 
-      const fetchData = await fetch("http://localhost:3001/api/v1/user/profile", {
-        method: "PUT",
-        headers: { "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-         },
-        body: JSON.stringify({ firstName, lastName }),
-      });
+    const fetchData = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ firstName, lastName }),
+    });
 
-      const response = await fetchData.json();
-      
-      if (response.status === 200 && response.body.token) {
-        dispatch(setSuccessUpdate(true));
-        dispatch(setProfile(response.body));
-      }
+    const response = await fetchData.json();
 
-      else if (response.status !== 200 && response.body.token) dispatch(setError(response.status))
-    
-    } catch (error) {
-      dispatch(setErrorUpdate(error.toString()));
-      dispatch(setError(error.toString()));
+    if (response.status === 200 && response.body) {
+      dispatch(setSuccessUpdate(true));
+      dispatch(setProfile(response.body));
     }
+
+    else if (response.status !== 200 && response.body.token) dispatch(setError(response.status))
+
+  } catch (error) {
+    dispatch(setErrorUpdate(error.toString()));
+    dispatch(setError(error.toString()));
+  }
 }
 
-export const { setIsAuth, setToken, setProfile, setError, logout } = AuthSlice.actions;
-export const { selectIsAuth, selectToken, selectProfile, selectError, selectErrorUpdate, selectSuccessUpdate } = AuthSlice.selectors;
